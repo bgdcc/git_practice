daaf4e48c8b7e1983736cf7695c42b91213d6ac5
Bohdan Culea <bogdculea@gmail.com>
2024-09-02 22:49:30 +0200

    added bubble sort :)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] arr={1,4,2,6,44,86,33};
+
+        for (int i=0; i<arr.length-1;i++){
+            for (int j=0; j<arr.length-i-1; j++){
+                if(arr[j+1]<arr[j]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
     }
 }
